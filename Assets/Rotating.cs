cea773834b5b403104dad2f7a4ec9e97fad5f93c
vinz321 <vincenzo.micciche.27@gmail.com>
2022-11-30 08:58:58 +0100

    Initial Commit

---
+++ Assets/Rotating.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class Rotating : MonoBehaviour
+{
+    [SerializeField]
+    private ActionBasedController abc;
+    [SerializeField]
+    private Transform handle;
+    private bool anchored=false;
+    private Quaternion rotation=Quaternion.identity;
+    void Start()
+    {
+        abc=FindObjectOfType<ActionBasedController>();
+        abc.selectAction.action.performed+=Anchor;
+        abc.selectAction.action.canceled+=Detach;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(anchored){
+            transform.LookAt(handle);
+            transform.rotation*=rotation;
+        }
+            
+    }
+
+    void Anchor(InputAction.CallbackContext context){
+        rotation=transform.rotation;
+        Debug.Log("Pressed");
+        anchored=true;
+    }
+    void Detach(InputAction.CallbackContext context){
+        Debug.Log("Detached");
+        anchored=false;
+    }
+
+    void FollowHandle(){
+        Vector3 position=abc.positionAction.action.ReadValue<Vector3>();
+        
+    }
+}
